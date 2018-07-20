4be9fa8027c47889a1c100a27843436c12c71cac
Guilherme Herzog <guilhermeherzog@gmail.com>
2018-07-20 23:18:18 +0200

    Fixing view.propTypes bug.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  View,
+  ViewPropTypes,
   WebView,
   StyleSheet,
 } from 'react-native';
@@ -20,7 +20,7 @@ class SignaturePad extends Component {
   static propTypes = {
     onChange: PropTypes.func,
     onError: PropTypes.func,
-    style: View.propTypes.style,
+    style: ViewPropTypes.style,
     penColor: PropTypes.string,
     dataURL: PropTypes.string,
   };
